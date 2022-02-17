96b4347d2aeb169186ab52d8b83d2c599375dd85
Joey Ciechanowicz <joeyciechanowicz@users.noreply.github.com>
2022-02-17 16:41:41 +0000

    str concat

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,29 @@
 import express from "express";
-import { renderToString } from "react-dom/server";
+import { renderToString, renderToNodeStream } from "react-dom/server";
 import React from "react";
 import { App } from "./app";
 
 const app = express();
 
-const pageTemplate = `<!DOCTYPE html>
+const pageTemplateHeader = `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8" />
     <title>How fast</title>
   </head>
-  <body>
-    #page-content
+  <body>`;
+
+const pageTemplateFooter = `
   </body>
 </html>
 `;
 
 app.get("/", (req, res) => {
   const appHTML = renderToString(<App />);
-  const html = pageTemplate.replace(
-    "#page-content",
-    `<div id="root">${appHTML}</div>`
-  );
-
   res.contentType("text/html");
   res.status(200);
 
-  return res.send(html);
+  return res.send(pageTemplateHeader + appHTML + pageTemplateFooter);
 });
 
 app.listen(8080, () => console.log("Listening on http://localhost:8080"));
